ec1d41833e184cbee5b9e2dd0eeac88c853f3426
gustavopertile <gu.follador@gmail.com>
2024-06-04 21:52:42 -0300

    make login first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tilapia_connect/firebase_options.dart';
-import 'package:tilapia_connect/pages/dashboard.dart';
 import 'pages/login_page.dart';
 
 void main() async {
@@ -22,8 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: const LoginPage(),
-      home: const Dashboard(),
+      home: const LoginPage(),
     );
   }
 }
